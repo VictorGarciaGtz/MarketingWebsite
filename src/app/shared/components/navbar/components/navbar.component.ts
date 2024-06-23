31dfd36af728a9bf528dd7fd53e04d84988fe3a3
Osempu <oscared94@outlook.com>
2024-06-22 19:03:55 -0500

    Added navbar & hero section in home

--- src/app/shared/components/navbar/components/navbar.component.ts
+++ src/app/shared/components/navbar/components/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, ElementRef, ViewChild, viewChild } from "@angular/core";
 
 @Component ({
   selector: 'mkt-navbar',
@@ -6,4 +6,20 @@ import { Component } from "@angular/core";
   styleUrls: ['./navbar.component.css']
 })
 
-export class NavbarComponent {}
+export class NavbarComponent {
+  @ViewChild('menuIcon') menuIcon!: ElementRef;
+  menu!: HTMLElement;
+
+  contructor() {
+  }
+
+  ngAfterViewInit(): void {
+    this.menu = this.menuIcon?.nativeElement;
+  }
+
+  // Function to Open Hamburguer menu and toggle menu icon
+  protected openMenu(): void {
+    this.menu.classList.toggle('la-times');
+    this.menu.classList.toggle('la-bars');
+  }
+}
